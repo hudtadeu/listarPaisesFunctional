d2dff298008b1565eb63e239b06a67a4a98ca6fd
Hudson Tadeu <hudinvesting@gmail.com>
2022-10-27 18:54:38 -0300

    Conversão do componente Country

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import css from './countries/countries.module.css';;
 
-export default class Country extends Component {
-  render() {
-    const { country } = this.props;
-    const { name, flag } = country;
+export default function Country({ country }) {
 
-    return (
-      <div className={`${css.country} ${css.border}`}>
-        <img className={css.flag} src={flag} alt={name} />
-        <span className={css.countryName}>{name}</span>
-      </div>
-    )
-  }
+  const { name, flag } = country;
+
+  return (
+    <div className={`${css.country} ${css.border}`}>
+      <img className={css.flag} src={flag} alt={name} />
+      <span className={css.countryName}>{name}</span>
+    </div>
+  )
 }
